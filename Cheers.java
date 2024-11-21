243b8a9badbcce3722e486e6cb62ebff272633d3
adam11111111111111 <adamrieger116@gmail.com>
2024-11-21 08:36:30 +0200

    fix

--- Cheers.java
+++ Cheers.java
@@ -2,12 +2,17 @@
 public class Cheers {
         public static void main(String[] args) {
                 String word = args[0];
+                String newWord = "";
                 int count = Integer.parseInt(args[1]);
                 char letter;
                 String temp = "AEFHILMNORSX";
-                word = word.toUpperCase();
                 for (int i = 0; i < word.length(); i++) {
-                        letter = word.charAt(i);
+                        if (word.charAt(i) >= 'a' && word.charAt(i) <= 'z') {
+                                letter = (char) (word.charAt(i) - 32);
+                        } else {
+                                letter = word.charAt(i);
+                        }
+                        newWord += letter;
                         if (temp.indexOf(letter) != -1) {
                                 System.out.println("Give me an " + letter + ":  " + letter + "!");
                         } else {
@@ -17,7 +22,7 @@ public class Cheers {
                 }
                 System.out.println("what does that spell?");
                 for (int i = 0; i < count; i++) {
-                        System.out.println(word + "!!!");
+                        System.out.println(newWord + "!!!");
                 }
         }
 }
